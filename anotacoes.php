02302f0c75a9be7228d26b725506d494facfc218
DaniloDuarte <danilojoseduarte@gmail.com>
2023-02-14 15:34:10 -0300

    Aula php concluída

--- anotacoes.php
+++ anotacoes.php
@@ -1,6 +1,9 @@
-Anotações aula "Por Dentro PHP"
+<!-- Anotações aula "Por Dentro PHP"
 
 Delimitacao - SuperTag
 <?php 
     
 ?>
+
+Anotações aula "Configurando php.ini" -->
+<!-- Configuração realizada -->
