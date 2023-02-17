88e83538e15069d4588a068a4aebc9b80bd2356d
DaniloDuarte <danilojoseduarte@gmail.com>
2023-02-17 12:30:30 -0300

    Aula PHP Variaveis e Constantes

--- anotacoes.php
+++ anotacoes.php
@@ -7,3 +7,12 @@ Delimitacao - SuperTag
 
 Anotações aula "Configurando php.ini" -->
 <!-- Configuração realizada -->
+
+<!-- Aula declarações variaveis & constantes -->
+
+
+    <?echo
+    $nome = "Danilo";
+    $sobrenome = "Duarte";
+
+    echo "Muito prazer, $nome, #sobrenome!";
