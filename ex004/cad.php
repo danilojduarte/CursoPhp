c6144e75b5e49b09cd01d16fec589c15e927d088
DaniloDuarte <danilojoseduarte@gmail.com>
2023-03-21 15:15:06 -0300

    Aula concluída

--- ex004/cad.php
+++ ex004/cad.php
@@ -4,16 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../ex004/style.css">
     <title>Resultado</title>
 </head>
 <body>
+
+        <!-- function ($_REQUEST) = è uma junção de $_GET | $_POST | $_COOKIES -->
+
     <header>
         <h1>Resultado do processamento</h1>
     </header>
     <main>
         <?php 
-            
+            $nome = $_GET("nome");
+            $s = $_GET("sobrenome");
+            echo "<pÉ um prazer te conhecer, $nome $s! Este é o meu site!</p>"
         ?>
     </main>
     
